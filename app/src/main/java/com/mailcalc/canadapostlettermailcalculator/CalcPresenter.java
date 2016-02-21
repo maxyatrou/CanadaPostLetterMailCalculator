89334796a945dcca3a6edbfc0cdd3880adad8440
sebastien177 <sebastien.arrese@gmail.com>
2016-02-20 20:43:29 -0500

    Fixed Error bug

--- app/src/main/java/com/mailcalc/canadapostlettermailcalculator/CalcPresenter.java
+++ app/src/main/java/com/mailcalc/canadapostlettermailcalculator/CalcPresenter.java
@@ -19,19 +19,50 @@ public class CalcPresenter {
             dontCalc = true;
         }
 
+        else {
+            int weightValue = Integer.parseInt(weight);
+            if (weightValue < 2 || weightValue > 500) {
+                view.showWeightError(R.string.weight_outofbounds);
+                dontCalc = true;
+            }
+        }
+
         if(length.isEmpty()) {
             view.showLengthError(R.string.length_empty);
             dontCalc = true;
+        }
+        else{
+            int lengthValue = Integer.parseInt(length);
+            if(lengthValue < 140 || lengthValue > 380) {
+                view.showLengthError(R.string.length_outofbounds);
+                dontCalc = true;
+            }
         }
 
         if(width.isEmpty()) {
             view.showWidthError(R.string.width_empty);
             dontCalc = true;
         }
+        else{
+            int widthValue = Integer.parseInt(width);
+            if(widthValue < 90 || widthValue > 270) {
+                view.showWidthError(R.string.width_outofbounds);
+                dontCalc = true;
+            }
+
+        }
 
         if(thickness.isEmpty()) {
             view.showThicknessError(R.string.thickness_empty);
             dontCalc = true;
+        }
+        else{
+            double thicknessValue = Double.parseDouble(thickness);
+            if(thicknessValue < 0.18 || thicknessValue > 20) {
+                view.showThicknessError(R.string.thickness_outofbounds);
+                dontCalc = true;
+            }
+
         }
 
         if(destination.equals("Destination")) {
@@ -47,36 +78,13 @@ public class CalcPresenter {
         if(dontCalc) {
             return;
         }
+        else {
+            int weightValue = Integer.parseInt(weight);
+            int lengthValue = Integer.parseInt(length);
+            int widthValue = Integer.parseInt(width);
+            double thicknessValue = Double.parseDouble(thickness);
 
-        int weightValue = Integer.parseInt(weight);
-        int lengthValue = Integer.parseInt(length);
-        int widthValue = Integer.parseInt(width);
-        double thicknessValue = Double.parseDouble(thickness);
-
-        if(weightValue < 2 || weightValue > 500) {
-            view.showWeightError(R.string.weight_outofbounds);
-            dontCalc = true;
+            double calcRate = service.calc(weightValue, lengthValue, widthValue, thicknessValue, destination, typeOfItem);
         }
-
-        if(lengthValue < 140 || lengthValue > 380) {
-            view.showLengthError(R.string.length_outofbounds);
-            dontCalc = true;
-        }
-
-        if(widthValue < 90 || widthValue > 270) {
-            view.showWidthError(R.string.width_outofbounds);
-            dontCalc = true;
-        }
-
-        if(thicknessValue < 0.18 || thicknessValue > 20) {
-            view.showThicknessError(R.string.thickness_outofbounds);
-            dontCalc = true;
-        }
-
-        if(dontCalc) {
-            return;
-        }
-
-        double calcRate = service.calc(weightValue,lengthValue,widthValue,thicknessValue, destination, typeOfItem);
     }
 }
